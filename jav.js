7b0c9929a7e95596a7357675b64b3407b3e5fbad
Gabriel Kruger <gabrielkr32@gmail.com>
2023-12-01 21:57:18 -0300

    modificação

--- jav.js
+++ jav.js
@@ -1,3 +1,4 @@
+
 var listaFilmes = ["https://br.web.img3.acsta.net/c_310_420/medias/nmedia/18/90/07/53/20391069.jpg", "https://br.web.img2.acsta.net/pictures/23/05/08/10/29/0695770.jpg", "https://br.web.img3.acsta.net/pictures/23/06/14/20/18/3063969.jpg", "https://cinebuzz.uol.com.br/media/uploads/terrifier_2_poster_oficial.jpg", "https://www.cafecomfilme.com.br/media/k2/items/cache/4c7ab8f301ba54865c7a2b6f3f6e590d_XL.jpg?t=20210322_175342" ];
 for (var i = 0; i<listaFilmes.length; i=i+1){
   document.write("<img src= " + listaFilmes[i] + ">")
